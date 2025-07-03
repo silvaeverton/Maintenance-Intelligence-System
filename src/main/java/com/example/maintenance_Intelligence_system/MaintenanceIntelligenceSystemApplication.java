e03e237e09a57f9d12072b8883792f17154efa60
silvaeverton <dev.evertonrodrigosilva@gmail.com>
2025-07-03 15:30:49 -0300

    Feat:Subindo projeto

---
+++ src/main/java/com/example/maintenance_Intelligence_system/MaintenanceIntelligenceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenance_Intelligence_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceIntelligenceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceIntelligenceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maintenance_Intelligence_system/MaintenanceIntelligenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenance_Intelligence_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceIntelligenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
